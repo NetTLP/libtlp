15fda10677190537feacacf90d2012eab75e9894
Ryo Nakamura <upa@haeena.net>
2020-02-11 18:53:24 +0900

    add apps/example.c

---
+++ apps/example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <libtlp.h>
+
+
+int main(int argc, char **argv)
+{
+	int ret;
+	char buf[128];
+	uintptr_t addr = 0x0;
+	struct nettlp nt;
+
+	inet_pton(AF_INET, "192.168.10.1", &nt.remote_addr);
+	inet_pton(AF_INET, "192.168.10.3", &nt.local_addr);
+	nt.requester = (0x1A << 8 | 0x00);
+	nt.tag = 0;
+
+	nettlp_init(&nt);
+
+	ret = dma_read(&nt, addr, buf, sizeof(buf));
+	printf("DMA read: %d bytes from 0x%lx\n", ret, addr);
+}
